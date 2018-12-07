93790326686dea3ba728b46cfd1706f45c24ec89
Jakub <j.perich@seznam.cz>
2018-12-07 22:58:40 +0700

    First version of Harold Shows

--- haroldWatchesShows/ViewController.swift
+++ haroldWatchesShows/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showsLabel: UILabel!
+    @IBOutlet weak var randomShowLabel: UILabel!
+    @IBOutlet weak var addShowTextfield: UITextField!
+    @IBOutlet weak var haroldSpoken: UILabel!
+    @IBOutlet weak var addShowButton: UIButton!
+    @IBOutlet weak var addShowStackviews: UIStackView!
+    @IBOutlet weak var firstShowsStackview: UIStackView!
+    @IBOutlet weak var whatBingeButton: UIButton!
+    @IBOutlet weak var randomShowStackview: UIStackView!
+    
+    var shows: [String] = []
+    
+
+    func updateShowsLabel() {
+        showsLabel.text = shows.joined(separator: ", ")
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        randomShowStackview.isHidden = true
+        firstShowsStackview.isHidden = true
+        whatBingeButton.isHidden = true
     }
 
+    
+    
+    @IBAction func addShowButtonWasPressed(_ sender: Any) {
+        guard let showName =  addShowTextfield.text
+            else
+        {return}
+        
+        if !showName.isEmpty{
+        shows.append(showName)
+        updateShowsLabel()
+        addShowTextfield.text = ""
+        firstShowsStackview.isHidden = false
+        if shows.count > 1 {
+        whatBingeButton.isHidden = false
+        }
+        }
+    }
+    
+    
+    
+    @IBAction func whatBingeButtonPressed(_ sender: Any) {
+         randomShowLabel.text = shows[Int.random(in: 0..<shows.count)]
+        randomShowStackview.isHidden = false
 
+    }
+    
+  
+
+    
 }
 
